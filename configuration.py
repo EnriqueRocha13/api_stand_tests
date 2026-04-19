fd5561f11799b463e305a4e29b12752eaf23cb89
Enrique Dominguez <marcosenriquedominguezrocha@gmail.com>
2026-04-19 16:37:30 -0600

    Initial commit: Estructura base para pruebas de API

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-8e16dc9e-a714-4ae2-aa19-132e1c36c6ab.containerhub.tripleten-services.com/"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv" #como encontrar esto en la documentacion
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ test_main.py
@@ -0,0 +1,5 @@
+from sender_stand_request import get_docs
+
+
+def test_ejemplo():
+    get_docs()
